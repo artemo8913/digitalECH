c116a8aed6351e362ea8d2d462c3437acd1db293
LAPTOP-L9O08U7Q\kalin <kaline-son@ya.ru>
2021-10-07 00:39:32 +0700

    добавил в yarn типы papaparse

---
+++ Scripts/parsCSVtoTable.js
@@ -0,0 +1,7 @@
+const btnParseCsv = document.getElementById("btnParseCsv");
+btnParseCsv.onclick = parseCsv;
+
+function parseCsv(){
+    const csvFile = document.getElementById("csvFile").files[0];
+    Papa.
+}
